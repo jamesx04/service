2458970af7fe1352ab3b56a796caeae883a48b09
jamesx04 <jamesx04@users.noreply.github.com>
2020-07-29 11:21:22 +1000

    test lib import changes
    test lib import changes

--- example/simple/main.go
+++ example/simple/main.go
@@ -8,7 +8,7 @@ package main
 import (
 	"log"
 
-	"github.com/kardianos/service"
+	"github.com/jamesx04/service"
 )
 
 var logger service.Logger
--- service_su_test.go
+++ service_su_test.go
@@ -18,7 +18,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/kardianos/service"
+	"github.com/jamesx04/service"
 )
 
 const runAsServiceArg = "RunThisAsService"
